604c380e744c3f11d3a204c210854c38b1a9a298
Karlo Sebalj <karlo.sebalj@greyp.com>
2020-08-21 17:38:03 +0200

    Concurrency errors

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,17 @@
 // 2. function object
 // 3. Lambda functions
 
-void displayFirstThread()
+void displayThread()
 {
     for (size_t i = 0; i < ITERATION_CYCLE; i++)
     {
-        std::cout << "I am a first worker Thread!" << std::endl;
+        std::cout << "Worker with id: " << std::this_thread::get_id() << " is executing!" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
         // std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
 
-void displaySecontThread()
-{
-    for (size_t i = 0; i < ITERATION_CYCLE; i++)
-    {
-        std::cout << "I am a second worker Thread!" << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-}
+
 
 // 2. Function Object 
 class DisplayThread
@@ -42,56 +35,21 @@ public:
 
 
 int main()
-{
-    // 1. function pointer
-    std::thread firstWorker(displayFirstThread);
-    std::thread secondWorker(displaySecontThread);
+{   
+    std::thread first(displayThread);
+    std::thread second(displayThread);
 
-    // 2. Function Object 
-    std::thread objectThread((DisplayThread()));
-
-    // 3. Lambda functions
-    std::thread lambdaThread([] {
-        for (size_t i = 0; i < ITERATION_CYCLE; i++)
-        {
-            std::cout << "I am a lambda Thread!" << std::endl;
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-
-        }
-    });
-
-    if (firstWorker.joinable())
+    if (second.joinable())
     {
-        std::cout << "First thread is ready to join" << std::endl;
-        firstWorker.join();
-    }
-    
-    if (secondWorker.joinable())
-    {
-        std::cout << "Second thread is ready to join" << std::endl;
-        secondWorker.join();
+       second.detach(); 
     }
 
-    if (objectThread.joinable())
+    if (second.joinable())
     {
-        std::cout << "Object thread is ready to join" << std::endl;
-        objectThread.join();
-    }
-    
-    if (lambdaThread.joinable())
-    {
-        std::cout << "Lambda thread is ready to join" << std::endl;
-        lambdaThread.join();
+       second.join(); 
     }
 
-    for (size_t i = 0; i < ITERATION_CYCLE; i++)
-    {
-        std::cout << "I am a main thread" << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-
-    }
-
-    // firstWorker.join();
-    // secondWorker.join();
-    return 0;
+    // first.join();
+    // second.join();
+    std::cin.get();
 }
